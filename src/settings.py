878f5a65e16fdd511e202d1d07c8f7daca5821ee
Olekseii Martseniuk <martsenyk.oleksii@ezcloudllc.com>
2023-10-09 11:54:20 +0300

    init

---
+++ src/settings.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
