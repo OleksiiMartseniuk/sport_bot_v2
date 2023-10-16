740c6bb4bb83f5cc8638ecfd0db776e4eccbd91e
Olekseii Martseniuk <martsenyk.l@gmail.com>
2023-10-16 18:03:38 +0300

    add module bot

--- src/settings.py
+++ src/settings.py
@@ -6,9 +6,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 IMAGES_DIR = BASE_DIR / "media/images"
 
+# Data Base
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
@@ -19,3 +21,6 @@ DATABASE_URL_ASYNC = (
     f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@"
     f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 )
+
+# Telegram
+BOT_TOKEN = os.getenv("BOT_TOKEN")
