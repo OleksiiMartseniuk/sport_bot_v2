924e76b952c7285c0d07877ff650f562edaa53f5
Olekseii Martseniuk <martsenyk.l@gmail.com>
2023-10-21 14:43:01 +0300

    add dependency SqlAlchemyUnitOfWork

--- src/bot/main.py
+++ src/bot/main.py
@@ -3,10 +3,12 @@ from aiogram.enums import ParseMode
 
 from src.settings import BOT_TOKEN
 from src.bot.handlers.program import program_router
+from src.utils.unitofwork import SqlAlchemyUnitOfWork
 
 
 async def main() -> None:
     dp = Dispatcher()
+    dp["uow"] = SqlAlchemyUnitOfWork()
 
     dp.include_routers(
         program_router,
