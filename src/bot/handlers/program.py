521691e479775554597d37a8a19d7bbf5f4092ef
Olekseii Martseniuk <martsenyk.l@gmail.com>
2023-10-22 12:11:40 +0300

    add handler program

--- src/bot/handlers/program.py
+++ src/bot/handlers/program.py
@@ -1,15 +1,33 @@
-from aiogram import Router
-from aiogram.types import Message
+from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery
+
+from src.utils.unitofwork import SqlAlchemyUnitOfWork
+from src.bot.callback.program import ProgramCallback, MenuLevels
+from src.bot.keyboards.inline.program import ProgramKeyboard
 
 
 program_router = Router()
 
 
 @program_router.message()
-async def get_category(message: Message):
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+async def categories_view(message: Message, uow: SqlAlchemyUnitOfWork):
+    categories_keyboard = await ProgramKeyboard.get_categories(uow=uow)
+    await message.answer("Categories", reply_markup=categories_keyboard)
+
+
+@program_router.callback_query(
+    ProgramCallback.filter(F.menu_level == MenuLevels.program)
+)
+async def programs_view(
+    query: CallbackQuery,
+    callback_data: ProgramCallback,
+    uow: SqlAlchemyUnitOfWork,
+):
+    programs_keyboard = await ProgramKeyboard().get_program(
+        uow=uow,
+        category_id=callback_data.category,
+    )
+    await query.message.edit_text(
+        text="Programs",
+        reply_markup=programs_keyboard,
+    )
