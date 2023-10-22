479eb264a3699d1a9f51342398b69514d9516acc
Olekseii Martseniuk <martsenyk.l@gmail.com>
2023-10-22 11:53:32 +0300

    add callback

---
+++ src/bot/callback/program.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+from aiogram.filters.callback_data import CallbackData
+
+
+class MenuLevels(str, Enum):
+    category = "category"
+    program = "program"
+    day = "day"
+
+
+class ProgramCallback(CallbackData, prefix="program"):
+    menu_level: MenuLevels
+    category: int
+    program: int
+    day: int
+    exercise: int
