727478ebf7441110c6f0624bab76161240aa314c
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-11-18 12:44:55 +0200

    fix models

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,4 +1,4 @@
 from .base import Base
-from .models.user import User
+from .models.user import TelegramUser
 from .models.program import Category, Program, Exercise
 from .models.history import HistoryExercise
