dc4e5e9657341c1dc55ff0cf16885c53330fff62
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-11-19 10:57:41 +0200

    update

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,4 +1,5 @@
 from .base import Base
-from .models.user import TelegramUser
+from .models.user import TelegramUser, User
 from .models.program import Category, Program, Exercise
 from .models.history import HistoryExercise
+from .models.token import Token
