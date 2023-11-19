d3a93beba955922bd4b42d6e4cb8e6afa62efabe
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-11-19 10:55:47 +0200

    add service commands

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import click
 
 from src.bot.main import main as bot
 from src.services.import_data import ImportDataService
+from src.services.commands import Commands
 from src.settings import BASE_DIR
 
 
@@ -20,6 +21,14 @@ def run_bot():
 @click.argument("patch", type=click.Path(exists=True), required=True)
 def write_programs(patch: str):
     asyncio.run(ImportDataService().import_file(path=(BASE_DIR / patch)))
+
+
+@cli.command()
+@click.argument("username", type=click.STRING, required=True)
+@click.argument("password", type=click.STRING, required=True)
+def create_staff(username: str, password: str):
+    asyncio.run(Commands.create_staff(username, password))
+    click.echo(f"User {username} created")
 
 
 if __name__ == "__main__":
