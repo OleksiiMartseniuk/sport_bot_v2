d2f3c6c90052b7bc14292a9f9f767f79702128c7
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-11-21 21:41:06 +0200

    update command name

--- src/services/commands.py
+++ src/services/commands.py
@@ -3,7 +3,7 @@ from src.utils.unitofwork import SqlAlchemyUnitOfWork
 
 class Commands:
     @staticmethod
-    async def create_staff(username: str, password: str) -> None:
+    async def create_user_staff(username: str, password: str) -> None:
         uow = SqlAlchemyUnitOfWork()
         async with uow:
             await uow.user.create_staff(username=username, password=password)
