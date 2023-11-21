8e512468b2673a8e0895d92f96c5e128241bbc50
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-11-21 20:44:41 +0200

    update AdminAuth

--- src/app/admin/auth.py
+++ src/app/admin/auth.py
@@ -1,20 +1,34 @@
 from sqladmin.authentication import AuthenticationBackend
 from starlette.requests import Request
+from src.services.hash import Hasher
+from src.settings import SECRET_KEY
 
 
 class AdminAuth(AuthenticationBackend):
     async def login(self, request: Request) -> bool:
-        # form = await request.form()
-        # username, password = form["username"], form["password"]
-
-        # Validate username/password credentials
-        # And update session
-        request.session.update({"token": "..."})
-
+        form = await request.form()
+        username, password = form["username"], form["password"]
+        uow = request.state.uow
+        user = await uow.user.get_or_none(username=username)
+        if user and user.is_staff:
+            is_hash_password = Hasher.verify_password(
+                plain_password=password,
+                hashed_password=user.password,
+            )
+            if is_hash_password is False:
+                return False
+        else:
+            return False
+        token = await uow.token.get_or_create(user_id=user.id)
+        await uow.commit()
+        request.session.update({"token": token.token})
         return True
 
     async def logout(self, request: Request) -> bool:
-        # Usually you'd want to just clear the session
+        token = request.session.get("token")
+        if token:
+            await request.state.uow.token.delete(token=token)
+            await request.state.uow.commit()
         request.session.clear()
         return True
 
@@ -24,8 +38,10 @@ class AdminAuth(AuthenticationBackend):
         if not token:
             return False
 
-        # Check the token in depth
+        token = await request.state.uow.token.get_or_none(token=token)
+        if token is None:
+            return False
         return True
 
 
-authentication_backend = AdminAuth(secret_key="...")
+authentication_backend = AdminAuth(secret_key=SECRET_KEY)
