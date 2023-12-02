6ecbf29b48e34f34c3aed67889ea8b4f17fb44db
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-12-02 19:55:34 +0200

    add model StatusLog

---
+++ src/database/models/logger.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from datetime import datetime
+
+from sqlalchemy import text
+from sqlalchemy import String, Enum
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from src.database import Base
+from src.utils.utils import LogLevel
+
+
+class StatusLog(Base):
+    __tablename__ = "status_log"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    logger_name: Mapped[str] = mapped_column(String(255))
+    level: Mapped[Enum] = mapped_column(Enum(LogLevel))
+    msg: Mapped[str] = mapped_column(String)
+    trace: Mapped[Optional[str]] = mapped_column(String)
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=text("TIMEZONE('utc', now())"),
+    )
+
+    def __repr__(self):
+        return f"Log: {self.level} - {self.msg}"
