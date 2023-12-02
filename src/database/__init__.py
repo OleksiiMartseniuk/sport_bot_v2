6a087f814517839be9ca2c1c3e372c2e3600755a
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-12-02 19:56:34 +0200

    add model StatusLog

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -3,3 +3,4 @@ from .models.user import TelegramUser, User
 from .models.program import Category, Program, Exercise
 from .models.history import HistoryExercise
 from .models.token import Token
+from .models.logger import StatusLog
