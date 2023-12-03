fefb7a1e30ab0a8e464549a572347e8756ff59f0
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-12-03 21:37:34 +0200

    database entry

--- src/db_logger/db_log_handler.py
+++ src/db_logger/db_log_handler.py
@@ -1,12 +1,15 @@
 import logging
 
+from src.database.base import sync_session
+from src.database.models.logger import StatusLog
+from src.utils.utils import LogLevel
+
+
 db_default_formatter = logging.Formatter()
 
 
 class DatabaseLogHandler(logging.Handler):
     def emit(self, record):
-        from src.database.models.logger import StatusLog
-
         trace = None
 
         if record.exc_info:
@@ -15,9 +18,11 @@ class DatabaseLogHandler(logging.Handler):
 
         kwargs = {
             'logger_name': record.name,
-            'level': record.levelno,
+            'level': LogLevel(record.levelno),
             'msg': msg,
             'trace': trace
         }
 
-        StatusLog(**kwargs)
+        with sync_session() as session:
+            session.add(StatusLog(**kwargs))
+            session.commit()
