fae3835a13fdbea3ddbe32be3b1e071c1114fea6
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-12-03 21:36:30 +0200

    add connect sync sqlalchemy

--- src/database/base.py
+++ src/database/base.py
@@ -1,12 +1,18 @@
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker
 
-from src.settings import DATABASE_URL_ASYNC
+from src.settings import DATABASE_URL_ASYNC, DATABASE_URL_SYNC
 
+# Sync
+engin_sync = create_engine(DATABASE_URL_SYNC)
+sync_session = sessionmaker(engin_sync)
 
-engine = create_async_engine(DATABASE_URL_ASYNC)
-async_session = async_sessionmaker(engine, expire_on_commit=False)
+# Async
+engine_async = create_async_engine(DATABASE_URL_ASYNC)
+async_session = async_sessionmaker(engine_async, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
