6e1b93761d8ec0708c932623bb2c1d795bf62123
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-12-10 20:24:05 +0200

    change location schema File

--- src/services/import_data.py
+++ src/services/import_data.py
@@ -1,9 +1,33 @@
 import csv
 import logging
 
+from pydantic import BaseModel
+
 from src.utils.utils import download_image
 from src.utils.unitofwork import SqlAlchemyUnitOfWork
-from src.schemas.import_data import File
+from src.utils.utils import Week
+
+
+class File(BaseModel):
+    category: str
+    program: str
+    exercise_title: str
+    exercise_number_of_approaches: int
+    exercise_number_of_repetitions: int
+    exercise_rest: int
+    exercise_image_url: str
+    exercise_day: int
+
+    def get_fields_exercise(self):
+        exercise = {}
+        for key, value in self.model_dump().items():
+            if "exercise_image_url" in key:
+                continue
+            elif "exercise_day" in key:
+                exercise["day"] = Week(value)
+            elif "exercise_" in key:
+                exercise[key.replace("exercise_", "")] = value
+        return exercise
 
 
 class ImportDataService:
