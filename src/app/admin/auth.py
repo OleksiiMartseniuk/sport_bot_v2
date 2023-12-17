837e245771b676358ddac42b31c708e9f8dd07de
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-12-17 12:49:40 +0200

    update

--- src/app/admin/auth.py
+++ src/app/admin/auth.py
@@ -4,33 +4,37 @@ from sqladmin.authentication import AuthenticationBackend
 
 from src.services.hash import Hasher
 from src.settings import SECRET_KEY
+from src.utils.unitofwork import SqlAlchemyUnitOfWork
 
 
 class AdminAuth(AuthenticationBackend):
     async def login(self, request: Request) -> bool:
         form = await request.form()
         username, password = form["username"], form["password"]
-        uow = request.state.uow
-        user = await uow.user.get_or_none(username=username)
-        if user and user.is_staff:
-            is_hash_password = Hasher.verify_password(
-                plain_password=password,
-                hashed_password=user.password,
-            )
-            if is_hash_password is False:
+        uow = SqlAlchemyUnitOfWork()
+        async with uow:
+            user = await uow.user.get_or_none(username=username)
+            if user and user.is_staff:
+                is_hash_password = Hasher.verify_password(
+                    plain_password=password,
+                    hashed_password=user.password,
+                )
+                if is_hash_password is False:
+                    return False
+            else:
                 return False
-        else:
-            return False
-        token = await uow.token.get_or_create(user_id=user.id)
-        await uow.commit()
-        request.session.update({"token": token.token})
+            token = await uow.token.get_or_create(user_id=user.id)
+            await uow.commit()
+            request.session.update({"token": token.token})
         return True
 
     async def logout(self, request: Request) -> bool:
         token = request.session.get("token")
         if token:
-            await request.state.uow.token.delete(token=token)
-            await request.state.uow.commit()
+            uow = SqlAlchemyUnitOfWork()
+            async with uow:
+                await uow.token.delete(token=token)
+                await uow.commit()
         request.session.clear()
         return True
 
@@ -40,9 +44,11 @@ class AdminAuth(AuthenticationBackend):
         if not token:
             return False
 
-        token = await request.state.uow.token.get_or_none(token=token)
-        if token is None:
-            return False
+        uow = SqlAlchemyUnitOfWork()
+        async with uow:
+            token = await uow.token.get_or_none(token=token)
+            if token is None:
+                return False
         return True
 
 
