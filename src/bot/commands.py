b7bb7ddc9aa7bad83bbc5cbf4bce5bae588bbc79
Oleksii Martseniuk <martsenyk.l@gmail.com>
2024-01-20 10:28:05 +0200

    fix command error

--- src/bot/commands.py
+++ src/bot/commands.py
@@ -1,13 +1,20 @@
+import logging
+
 from aiogram import Bot
 from aiogram.types import BotCommand
 
+logger = logging.getLogger("db")
+
 
 async def set_commands(bot: Bot):
-    await bot.set_my_commands(
-        [
-            BotCommand(
-                command="program",
-                description="Программа тренировок",
-            ),
-        ]
-    )
+    try:
+        await bot.set_my_commands(
+            [
+                BotCommand(
+                    command="program",
+                    description="Программа тренировок",
+                ),
+            ]
+        )
+    except Exception as ex:
+        logger.error("Error setting commands", exc_info=ex)
