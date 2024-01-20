42eb7256ebd9f8b8407104433fb2322fa459559f
Oleksii Martseniuk <martsenyk.l@gmail.com>
2024-01-20 10:17:47 +0200

    add method get_image_telegram

--- src/repositories/project_settings.py
+++ src/repositories/project_settings.py
@@ -1,5 +1,15 @@
+from aiogram.types import InputMediaPhoto, FSInputFile
+
 from src.database.models.project_settings import ProjectSettings
 from src.utils.repository import SqlAlchemyRepository
+
+
+class ProjectSettingsError(Exception):
+    pass
+
+
+class ExistsProjectSettingsError(ProjectSettingsError):
+    pass
 
 
 class ProjectSettingsRepository(SqlAlchemyRepository[ProjectSettings]):
@@ -8,8 +18,24 @@ class ProjectSettingsRepository(SqlAlchemyRepository[ProjectSettings]):
     async def __check_exists(self):
         is_exists = await self.exists()
         if is_exists:
-            raise ValueError("ProjectSettings exists")
+            raise ExistsProjectSettingsError()
 
     async def create(self, data: dict) -> int:
         await self.__check_exists()
         return await super().create(data)
+
+    async def get_image_telegram(
+        self,
+        caption: str | None = None,
+    ) -> InputMediaPhoto:
+        project_settings = await self.get()
+        if project_settings.menu_image_telegram_id:
+            return InputMediaPhoto(
+                media=project_settings.menu_image_telegram_id,
+                caption=caption,
+            )
+        else:
+            return InputMediaPhoto(
+                media=FSInputFile(path=project_settings.menu_image_path),
+                caption=caption,
+            )
