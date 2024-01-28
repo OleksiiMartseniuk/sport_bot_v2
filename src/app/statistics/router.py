aaac94fa56af98e1c6e001ebd3449d990eba2d35
Oleksii Martseniuk <martsenyk.l@gmail.com>
2024-01-28 19:28:47 +0200

    add ResponseSchema

--- src/app/statistics/router.py
+++ src/app/statistics/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from src.app.statistics.schemas import HistoryExercisesSchema
+from src.app.utils import ResponseSchema, pagination
 from src.utils.unitofwork import SqlAlchemyUnitOfWork, get_sql_alchemy_unit_of_work
 
 router = APIRouter(prefix="/statistics")
@@ -9,12 +10,18 @@ router = APIRouter(prefix="/statistics")
 @router.get("/list/{telegram_user_id}/")
 async def get_statistics(
     telegram_user_id: int,
+    pagination_params: dict[str, int] = Depends(pagination),
     uow: SqlAlchemyUnitOfWork = Depends(get_sql_alchemy_unit_of_work),
-) -> list[HistoryExercisesSchema]:
+) -> ResponseSchema[HistoryExercisesSchema]:
     history_list = await uow.history_exercise.get_statistics(
-        telegram_user_id=telegram_user_id,
+        telegram_user_id=telegram_user_id, **pagination_params
     )
-    return [
+    history_count = await uow.history_exercise.count(telegram_user_id=telegram_user_id)
+    history_list_data = [
         HistoryExercisesSchema.model_validate(h, from_attributes=True)
         for h in history_list
     ]
+    return ResponseSchema[HistoryExercisesSchema](
+        response=history_list_data,
+        count=history_count,
+    )
