b6ee4d139aa2c0a452747621a056169facd20d9e
Oleksii Martseniuk <martsenyk.l@gmail.com>
2024-01-28 13:13:25 +0200

    add get_statistics

--- src/repositories/history.py
+++ src/repositories/history.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 
-from sqlalchemy import select, func
+from sqlalchemy import func, select
+from sqlalchemy.orm import selectinload
 
 from src.database.models.history import HistoryExercise
+from src.database.models.program import Program
 from src.utils.repository import SqlAlchemyRepository
 
 
@@ -10,8 +12,23 @@ class HistoryExerciseRepository(SqlAlchemyRepository[HistoryExercise]):
     model = HistoryExercise
 
     async def get_count_history_today(self, **filters) -> int:
-        stmt = select(func.count(self.model.id)).filter_by(**filters).where(
-            func.date(self.model.created_at) == datetime.utcnow().date()
+        stmt = (
+            select(func.count(self.model.id))
+            .filter_by(**filters)
+            .where(func.date(self.model.created_at) == datetime.utcnow().date())
         )
         res = await self.session.execute(stmt)
         return res.scalar_one()
+
+    async def get_statistics(self, telegram_user_id: int, **filters):
+        stmt = (
+            select(self.model)
+            .options(
+                selectinload(self.model.exercise),
+                selectinload(self.model.program).selectinload(Program.category),
+            )
+            .filter_by(**filters)
+            .where(self.model.telegram_user_id == telegram_user_id)
+        )
+        res = await self.session.execute(stmt)
+        return res.scalars().all()
